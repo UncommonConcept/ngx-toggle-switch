ab9224722283a82a4135746cd56da193d5b3df77
Dory Zidon <dory.zidon@gmail.com>
2018-08-12 18:04:24 -0400

    added build

---
+++ dist/es2015/public_api.js
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+export { UiSwitchComponent } from "./ui-switch/ui-switch.component";
+export { UiSwitchModule } from "./ui-switch/ui-switch.module";
+//# sourceMappingURL=public_api.js.map
---
+++ dist/es5/ui-switch/ui-switch.module.js
@@ -0,0 +1,41 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { UiSwitchComponent } from "./ui-switch.component";
+var UiSwitchModule = (function () {
+    function UiSwitchModule() {
+    }
+    UiSwitchModule.decorators = [
+        { type: NgModule, args: [{
+                    declarations: [
+                        UiSwitchComponent
+                    ],
+                    imports: [
+                        CommonModule,
+                        FormsModule
+                    ],
+                    exports: [
+                        FormsModule,
+                        UiSwitchComponent
+                    ]
+                },] },
+    ];
+    /** @nocollapse */
+    UiSwitchModule.ctorParameters = function () { return []; };
+    return UiSwitchModule;
+}());
+export { UiSwitchModule };
+function UiSwitchModule_tsickle_Closure_declarations() {
+    /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+    UiSwitchModule.decorators;
+    /**
+     * @nocollapse
+     * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+     */
+    UiSwitchModule.ctorParameters;
+}
+//# sourceMappingURL=ui-switch.module.js.map
